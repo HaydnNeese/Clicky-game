b9e42ae371dbd61c5078bb851097784cfdaf44fd
HaydnNeese <haydnneese@yahoo.com>
2019-04-13 13:54:54 -0500

    Updates

---
+++ precache-manifest.b6e22a97c7a1269adf1a90f7334db7b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cda0924bd23e3d9029fa",
+    "url": "/Clicky-game/static/js/runtime~main.952307d2.js"
+  },
+  {
+    "revision": "897b501af3cd046d1bb1",
+    "url": "/Clicky-game/static/js/main.ca9f4319.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "897b501af3cd046d1bb1",
+    "url": "/Clicky-game/static/css/main.6bac8e1b.chunk.css"
+  },
+  {
+    "revision": "4373eb92325ed068996c009c8b39cf8d",
+    "url": "/Clicky-game/index.html"
+  }
+];
